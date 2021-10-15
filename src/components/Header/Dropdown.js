cbdff73691d95498a8a76249a3fefae0d42244fc
Brian Kwok <brkwok@gmail.com>
2021-10-14 20:51:05 -0400

    Finish dropdown for sorting algorithms

--- src/components/Header/Dropdown.js
+++ src/components/Header/Dropdown.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Dropdown() {
   return (
-    <div id="header-dropdown-fields">
-      <div id="header-algo-list">
-        <div><span>Bubble Sort</span></div>
+    <div id="header-dropdown-fields" className="header-dropdown-fields">
+      <div id="header-dropdown-algo-list">
+        <div>Bubble Sort</div>
         <div>Selection Sort</div>
         <div>Quick Sort</div>
         <div>Merge Sort</div>
